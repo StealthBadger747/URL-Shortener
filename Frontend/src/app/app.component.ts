919b6898982544a482d2ae7379af132dcb986122
Erik Parawell <elparawell@dons.usfca.edu>
2021-12-04 17:23:30 -0800

    Created the frontend with the HTML

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'UrlShortener';
+  enteredURL: string = "";
+  shortenedURL: string = "";
+
+  constructor(public apiService: ApiService) { }
+
+  public postShortenURL(): void {
+    if (this.enteredURL.length === 0) {
+      return;
+    }
+
+    this.apiService.createShortURL(this.enteredURL).then((value: any) => {
+      this.shortenedURL = window.location.origin + "/" + value.short_url;
+    });
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # This is a URL Shortener
+
+
+# Sources:
+- For the chain favicon: https://www.favicon-generator.org/search/---/Chain
